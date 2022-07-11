bbb6553bb0aec89ca03bacc3189fe5b05d2e5669
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-07-11 10:07:20 +0200

    finish: bonus 1

---
+++ BONUS 1/js/main.js
@@ -0,0 +1,55 @@
+const imagesList = [
+    "img/01.jpg",
+    "img/02.jpg",
+    "img/03.jpg",
+    "img/04.jpg",
+    "img/05.jpg",
+];
+
+const images = document.querySelector('.images');
+
+
+for (let i = 0; i < imagesList.length; i++) {
+    
+    images.innerHTML += `<div class="image">
+                            <img src="${imagesList[i]}" alt="photo 1">
+                        </div>`;
+}
+
+let activeImage = 0;
+
+const imgList = document.getElementsByClassName('image');
+
+imgList[activeImage].classList.add('show');
+
+const down = document.querySelector('.down');
+
+down.addEventListener('click',
+    function() {
+        
+        imgList[activeImage].classList.remove('show');
+        if(activeImage == imagesList.length -1) {
+            activeImage = 0;
+        } else {
+            activeImage++;
+        }
+        imgList[activeImage].classList.add('show');
+        console.log(activeImage);
+
+        
+    }
+)
+const up = document.querySelector('.up');
+
+up.addEventListener('click',
+    function() {
+        imgList[activeImage].classList.remove('show');
+        if(activeImage == 0) {
+            activeImage = imagesList.length;
+        } else {
+            activeImage--;
+        }
+        activeImage--;
+        imgList[activeImage].classList.add('show');
+    }
+)
